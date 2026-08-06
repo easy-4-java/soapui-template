49fc964a931ce395c303a51cfe2aeb3a6ef4a9e8
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 15:00:13 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/com/smartbear/soapui/template/wsdl/WsdlInterfaceInfo.java
+++ src/main/java/com/smartbear/soapui/template/wsdl/WsdlInterfaceInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -23,7 +23,7 @@ import com.eviware.soapui.impl.wsdl.WsdlOperation;
 import com.eviware.soapui.impl.wsdl.support.soap.SoapVersion;
 
 /**
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class WsdlInterfaceInfo {
 	
